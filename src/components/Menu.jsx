579d2be0686cafcf3a503fa7c0e114cec4d86f76
urisega <53956036+urisega@users.noreply.github.com>
2023-10-03 22:24:45 -0300

    Primer subida

---
+++ src/components/Menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Menu = () => {
+    /*spread operators
+    const numbers = [1,2,3];
+    const numbers2 = [4,5,6,10,15];
+    const arr3=[...numbers,...numbers2];
+    
+    function sum(x,y,z){
+        return x+y+z;
+    }
+    console.log(sum(...numbers));
+    console.log(arr3);
+    */
+
+
+    return (
+        <div>Menu</div>
+    )
+}
+
+export default Menu
